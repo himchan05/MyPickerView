baa3746a330f813143fcac9d8d28f147175d9e51
박힘찬 <himchan05@naver.com>
2022-03-15 21:08:54 +0900

    Initial Commit

---
+++ MyPickerView/MyPickerViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPickerViewApp.swift
+//  MyPickerView
+//
+//  Created by 박힘찬 on 2022/03/15.
+//
+
+import SwiftUI
+
+@main
+struct MyPickerViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
