e67cac4f05ce4a2c675996a9fb21f5a849403875
박힘찬 <himchan05@naver.com>
2022-03-15 21:44:22 +0900

    My Picker View

--- MyPickerView/ContentView.swift
+++ MyPickerView/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedValue: Int = 0
+    var myColor = ["오렌지색", "바나나색", "딸기색"]
+    func ChangeValue(index: Int) -> Color {
+        switch index {
+        case 0:
+            return Color.orange
+        case 1:
+            return Color.yellow
+        case 2:
+            return Color.red
+        default:
+            return Color.orange
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Circle()
+                .frame(width: 50, height: 50)
+                .foregroundColor(ChangeValue(index: selectedValue))
+            Text("세그먼트 value: \(selectedValue)")
+            Text("선택된 색상 : \(myColor[selectedValue])")
+            Picker("타이틀", selection: $selectedValue) {
+                Text("오렌지").tag(0)
+                Text("바나나").tag(1)
+                Text("딸기").tag(2)
+            }
+            .pickerStyle(.segmented)
+            
+            Picker("타이틀", selection: $selectedValue) {
+                Text("오렌지").tag(0)
+                Text("바나나").tag(1)
+                Text("딸기").tag(2)
+            }
+            .pickerStyle(.inline)
+        }
     }
 }
 
